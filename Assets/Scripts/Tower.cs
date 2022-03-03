a68f7dddbc4f2a17a5621799f9460a830f0619bf
Caspar Krieger <git@asparck.com>
2022-03-03 20:03:47 +0800

    Make towers shoot at enemies (but not hurt them yet)

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,30 +5,28 @@ public class Tower : GameTileContent {
     [SerializeField, Range(1.5f, 10.5f)]
     float targetingRange = 1.5f;
 
+    [SerializeField]
+    Transform turret = default, laserBeam = default;
+
     TargetPoint target;
 
     const int enemyLayerMask = 1 << 9;
 
     static Collider[] targetsBuffer = new Collider[1];
 
-    public override void GameUpdate() {
-        if (TrackTarget() || AcquireTarget()) {
-            Debug.Log("Locked on target");
-        }
+    Vector3 laserBeamScale;
+
+    private void Awake() {
+        laserBeamScale = laserBeam.localScale;
     }
 
-    private bool AcquireTarget() {
-        Vector3 a = transform.localPosition;
-        Vector3 b = a;
-        b.y += 3f;
-        int hits = Physics.OverlapCapsuleNonAlloc(a, b, targetingRange, targetsBuffer, enemyLayerMask);
-        if (hits > 0) {
-            target = targetsBuffer[0].GetComponent<TargetPoint>();
-            Debug.Assert(target != null, "Targeted non-enemy!", targetsBuffer[0]);
-            return true;
+    public override void GameUpdate() {
+        if (TrackTarget() || AcquireTarget()) {
+            Shoot();
         }
-        target = null;
-        return false;
+        else {
+            laserBeam.localScale = Vector3.zero;
+        }
     }
 
     bool TrackTarget() {
@@ -45,6 +43,31 @@ public class Tower : GameTileContent {
             return false;
         }
         return true;
+    }
+
+    private bool AcquireTarget() {
+        Vector3 a = transform.localPosition;
+        Vector3 b = a;
+        b.y += 3f;
+        int hits = Physics.OverlapCapsuleNonAlloc(a, b, targetingRange, targetsBuffer, enemyLayerMask);
+        if (hits > 0) {
+            target = targetsBuffer[0].GetComponent<TargetPoint>();
+            Debug.Assert(target != null, "Targeted non-enemy!", targetsBuffer[0]);
+            return true;
+        }
+        target = null;
+        return false;
+    }
+
+    private void Shoot() {
+        Vector3 point = target.Position;
+        turret.LookAt(point);
+        laserBeam.localRotation = turret.localRotation;
+
+        float d = Vector3.Distance(turret.position, point);
+        laserBeamScale.z = d;
+        laserBeam.localScale = laserBeamScale;
+        laserBeam.localPosition = turret.localPosition + 0.5f * d * laserBeam.forward;
     }
 
     private void OnDrawGizmosSelected() {
